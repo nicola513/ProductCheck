e22a914ce00aaf3dc9cb2786820d381910ce82a3
Nicola Ng <ngnicola513@gmail.com>
2026-01-22 16:55:20 -0800

    Add the HomeView

---
+++ ProductCheck/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  ProductCheck
+//
+//  Created by Nicola Ng on 22/1/2026.
+//
+
+import Foundation
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View{
+        Text("Hello, this is home page.")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
