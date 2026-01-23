f82527d54079a1ce6915092b057f0fa0741377d9
Nicola Ng <ngnicola513@gmail.com>
2026-01-22 16:08:24 -0800

    Initial Commit

---
+++ ProductCheck/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProductCheck
+//
+//  Created by Nicola Ng on 22/1/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProductCheck/ProductCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCheckApp.swift
+//  ProductCheck
+//
+//  Created by Nicola Ng on 22/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct ProductCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
